d20cd51d76f9c704ed17f674e7fa02983d267237
Muhammed Aslan <muhammed.aslan@glomil.com>
2024-09-10 20:37:00 +0300

    initial fix

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import banner from "../../public/medias/barcode-banner.png";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <main className="flex items-center justify-center w-full h-full">
+        <Image
+          className="m-auto"
+          src={banner}
+          alt="Next.js logo"
+          priority
+        />
+      </main>
+    </div>
+  );
+}
