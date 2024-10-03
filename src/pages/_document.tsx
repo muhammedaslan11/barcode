369ef7d334ecc06ebf7f38f2537a063858c5da48
Muhammed Aslan <muhammed.aslan@glomil.com>
2024-10-03 10:57:57 +0300

    feat: meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,13 +7,16 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Barcode Cosmetics - Professional Cosmetic Solutions" />
+        <meta name="theme-color" content="#000" />
         <meta name="keywords" content="cosmetics, beauty, skincare, Barcode, professional, Hairdresser" />
         <meta name="author" content="Barcode Cosmetics" />
+        <meta name="image" content="https://aslan.pockethost.io/api/files/xmfjzrnn6nsa9rs/lutmhc89is4dom9/barcode_dark_logo_JZBKJYibz3.png" />
         <meta property="og:title" content="Barcode Cosmetics - Professional Cosmetic Solutions" />
         <meta property="og:description" content="Discover professional cosmetic products with Barcode Cosmetics." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://barcodecosmetics.com" />
         <meta property="og:image" content="https://aslan.pockethost.io/api/files/xmfjzrnn6nsa9rs/lutmhc89is4dom9/barcode_dark_logo_JZBKJYibz3.png" />
+        <meta name="og:image" content="https://aslan.pockethost.io/api/files/xmfjzrnn6nsa9rs/lutmhc89is4dom9/barcode_dark_logo_JZBKJYibz3.png" />
         <meta name="twitter:card" content="https://aslan.pockethost.io/api/files/xmfjzrnn6nsa9rs/lutmhc89is4dom9/barcode_dark_logo_JZBKJYibz3.png" />
         <meta name="twitter:title" content="Barcode Cosmetics - Professional Cosmetic Solutions" />
         <meta name="twitter:description" content="Discover professional cosmetic products with Barcode Cosmetics." />
